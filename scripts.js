150efba7d5a6f3ceef4402d4eba41a8f2a971213
Chad90b <68355086+Chad90b@users.noreply.github.com>
2023-09-10 18:08:25 +0200

    fifth

--- scripts.js
+++ scripts.js
@@ -1,25 +1,25 @@
-    var lines;
-    var randomNumber;
-    var lastRandomNumber;
-    
-    $(document.body).ready(function () {
-      
-      $.ajax({
-        url: 'dadjokes.txt'
-      }).done(function(content) {
-        
-        lines = content.replace(/\r\n|\r/g, '\n').trim().split('\n');
-        
-        if (lines && lines.length) {
-           $('#showLine').ready( function() {
-            while (randomNumber === lastRandomNumber) {
-              randomNumber = parseInt(Math.random() * lines.length);
-              if (lines.length === 1) { break; }
-            }
-            lastRandomNumber = randomNumber;
+var lines;
+var randomNumber;
+var lastRandomNumber;
+
+	$(document.body).ready(function () {
+
+	$.ajax({
+		url: 'dadjokes.txt'
+	}).done(function(content) {
+
+	lines = content.replace(/\r\n|\r/g, '\n').trim().split('\n');
+
+	if (lines && lines.length) {
+		$('#showLine').ready( function() {
+			while (randomNumber === lastRandomNumber) {
+				randomNumber = parseInt(Math.random() * lines.length);
+				if (lines.length === 1) { break; }
+		}
+		lastRandomNumber = randomNumber;
             
-            $('#dadjoke').text(lines[randomNumber]);
-          });
-        }
-      });
-    });
+			$('#dadjoke').text(lines[randomNumber]);
+			});
+		}
+	});
+});
